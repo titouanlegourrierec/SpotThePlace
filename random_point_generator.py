5064b0057d98e8a21ebd46e35afb75449fba45ff
Titouan Le Gourrierec <titouanlegourrierec@icloud.com>
2024-11-12 23:03:46 +0100

    [refactor][doc] Merge 'plot_points_in_country' into 'generate_points_in_country' for better readability. Add documentation.

--- random_point_generator.py
+++ random_point_generator.py
@@ -1,17 +1,40 @@
+import random
+
 import geopandas as gpd
 from shapely.geometry import Point
-import random
+import matplotlib.pyplot as plt
 
 class RandomPointGenerator:
     SHAPEFILE_PATH = "ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp"
 
-    def __init__(self, random_state=1234):
+    def __init__(self, random_state: int = 1234):
         # Load the world shapefile
         self.world = gpd.read_file(RandomPointGenerator.SHAPEFILE_PATH)
         # Set the random state for reproducibility
         self.random_state = random_state
     
-    def generate_points_in_country(self, country_name: str, num_points: int) -> gpd.GeoDataFrame:
+    def generate_points_in_country(self, country_name: str, num_points: int,  markersize: float = 5, show: bool = True) -> gpd.GeoDataFrame:
+        """
+        Generates random points within the boundaries of a specified country and plots them.
+
+        This method filters the world shapefile to obtain the geometry of the specified country,
+        generates random points within the country's boundaries, and returns these points as a
+        GeoDataFrame with additional columns for latitude, longitude, and country name. It also
+        plots the generated points on the map of the specified country.
+
+        Parameters:
+            - country_name (str): The name of the country in which to generate points.
+            - num_points (int): The number of random points to generate.
+            - markersize (float): The size of the markers for the points. Defaults to 5.
+
+        Returns:
+            - gpd.GeoDataFrame: A GeoDataFrame containing the generated points with columns for
+                            'id', 'long', 'lat', and 'country'.
+
+        Example:
+            >>> generator = RandomPointGenerator()
+            >>> points_gdf = generator.generate_points_in_country('France', 100)
+        """
 
         random.seed(self.random_state)
 
@@ -19,7 +42,7 @@ class RandomPointGenerator:
         country = self.world[self.world['NAME'] == country_name]
 
         if country.empty:
-            raise ValueError(f"Le pays '{country_name}' n'a pas été trouvé dans le shapefile.")
+            raise ValueError(f"The country '{country_name}' was not found in the shapefile.")
         
         # Extract the bounds of the country's geometry
         minx, miny, maxx, maxy = country.total_bounds
@@ -40,14 +63,12 @@ class RandomPointGenerator:
         points_gdf["long"] = points_gdf.geometry.x
         points_gdf["lat"] = points_gdf.geometry.y
         points_gdf["country"] = country_name
-        
-        return points_gdf
 
-    def plot_points_in_country(self, country_name: str, num_points: int, markersize:float = 5) -> None:
-        # Generate random points in the specified country
-        points_gdf = self.generate_points_in_country(country_name, num_points)
-        
-        # Plot the country boundaries and the random points
-        ax = self.world[self.world['NAME'] == country_name].plot(color='#CACACA', edgecolor='black')
-        points_gdf.plot(ax=ax, color='red', markersize=markersize)
-        ax.set_axis_off()
+        if show:
+            # Plot the country boundaries and the random points
+            ax = country.plot(color='#CACACA', edgecolor='black')
+            points_gdf.plot(ax=ax, color='red', markersize=markersize)
+            ax.set_axis_off()
+            plt.show()
+
+        return points_gdf
